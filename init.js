33767127776bdcc6e0c8e609c5fc157e5f841878
Matthew Vickery <vickery.matthew@gmail.com>
2016-05-06 14:03:16 +0200

    removed unused files

--- init.js
+++
@@ -1,3 +0,0 @@
-if ("boolean" == typeof(_op_cor_settings_loaded)) { 
-    document.write('<s'+'cript src="//d3062rhc5ksps0.cloudfront.net/optcor.js" type="text/javascript">'+'<\/s'+'cript>');
-}
--- run.js
+++
@@ -1,6 +0,0 @@
-if ("boolean" == typeof(_op_cor_settings_loaded) && "function" == typeof(_op_cor_top_initialize)) {
-    _op_cor_top_initialize();
-    opc_$(document).ready(function() {
-        _op_cor_bottom_initialize();
-    });
-}
